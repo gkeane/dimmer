782a381ca6e57c94b77c376fda68739e8786770b
Greg Keane <gkeane@gmail.com>
2015-12-22 14:48:13 -0500

    to div

--- dimmer.php
+++ dimmer.php
@@ -1,9 +1,9 @@
-            
+<link rel="stylesheet" href="dimmer.css" type="text/css">
 <body>
 
     <form method="get" action="dimmer.php">
         
-        <select id="dimmer" name="dimmer">
+        <select class="dimmer" name="dimmer">
         
             <?php
             
@@ -33,17 +33,17 @@
                 </option>";
             
             }
-            echo "</select><input type=\"submit\" value=\"Submit\"></form>";
+            echo "</select><input type=\"submit\" value=\"Submit\"></form>\n";
             $dim = isset($_GET['dimmer']) ? $_GET['dimmer'] : 'A1';
             $dimmer= htmlspecialchars($dim);;
             $sql = "SELECT lamp,b.actual,refire_scale, calc_50, calc_20, lumen_watt, total FROM dimmer inner join led_names b on b.led=dimmer.lamp where dimmer='".$dimmer."'";
             //echo $sql;
             $result = mysql_query($sql) or die ("Query to get data from dimmer failed: ".mysql_error());
-            echo "<table>";
-            echo "<tr><td>Lamp</td><td>Lamp Actual</td><td>refire_scale</td><td>calc_50</td><td>calc_20</td><td>lumen_watt</td><td>total</td></tr>";
+            echo "<div class=\"rTable\">\n";
+            echo "<div class=\"rTableRow\"><div class=\"rTableHead\">Lamp</div><div class=\"rTableHead\">Lamp Actual</div><div class=\"rTableHead\">refire_scale</div><div class=\"rTableHead\">calc_50</div><div class=\"rTableHead\">calc_20</div><div class=\"rTableHead\">lumen_watt</div><div class=\"rTableHead\">total</div></div>\n";
                 // output data of each row
                 while($row = mysql_fetch_array($result)) {
-                    echo "<tr><td>" . $row["lamp"]. "</td><td>" . $row["actual"]. "</td><td>" . $row["refire_scale"]. "</td><td>". $row["calc_50"]. "</td><td>" . $row["calc_20"]. "</td><td>". $row["lumen_watt"]. "</td><td>". $row["total"]. "</td><td></tr>";
+                    echo "<div class=\"rTableRow\"><div class=\"rTableCell\">" . $row["lamp"]. "</div><div class=\"rTableCell\">" . $row["actual"]. "</div><div class=\"rTableCell\">" . $row["refire_scale"]. "</div><div class=\"rTableCell\">". $row["calc_50"]. "</div><div class=\"rTableCell\">" . $row["calc_20"]. "</div><div class=\"rTableCell\">". $row["lumen_watt"]. "</div><div class=\"rTableCell\">". $row["total"]. "</div></div>\n";
                 }
             echo "</table>";
             $conn->close();
