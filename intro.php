9081b1266ed9ee3efb5584574f4828fd998d1629
Greg Keane <gkeane@gmail.com>
2016-01-08 13:46:39 -0500

    change width

--- intro.php
+++ intro.php
@@ -1,14 +1,25 @@
 <?php include('header.html');?>
 
-
-<div style="clear: left;"></div>
+ <div style="height: auto; overflow: hidden;"></div>
+<div style="float: left; width: auto; overflow: hidden;">
     <h1>Introduction</h1>
     <p>As a variety of new poultry specific light emitting diode (LED) bulbs have become available, many broiler growers are considering replacing their older technology bulbs with LED bulbs to reduce energy costs. Most broiler grow-out programs use dimmers to reduce light intensity to maximize performance. Incandescent, cold cathode compact fluorescents, and LED bulbs respond differently to dimming and changes in bulb technology may require different dimmers or dimmer profiles/curves. Because of the large financial investment required to replace all the bulbs in a broiler house, broiler growers and integrators have been requesting lamp and dimmer information to assist in bulb, dimmer and dimmer profile selection. The purpose of this project was to provide an internet available tool for broiler growers and integrators to learn about the performance of specific bulbs when paired with specific dimmers.  There are many LED lamps that can work well in a poultry setting, however, there are some combinations of LED lamps and dimmers that are not well matched and should not be used.  Currently this project has evaluated the impact of 8 different commercial dimmers on the performance of 15 different LED bulbs. In addition, many of the dimmers have multiple dimmer profiles that were evaluated.  An integrated spectrometer system was used to individually evaluate each bulb, dimmer, and dimmer setting combination.  This testing system recorded input and output voltage, power consumption, luminous flux (light output) and spectral power distribution of the bulbs tested. These measurements were recorded for each combination of bulb, dimmer, and dimmer settings from 0 - 100% at 10% increments. </p>
-<p>Using the tabs at the top of the page you can access the scores of the bulb-dimmer interactions and view by bulb listing or by dimmer listing. The information can be sorted by column. Graphs of the light output curves, efficiency, and spectral power distribution (not yet available) of each bulb-dimmer combination are available by clicking the tab at the end of row of each table. Information on each bulb and dimmers is provided when available. There is also a tab at the top of the page to access a glossary of terms.</p>
+<p>Using the tabs at the top of the page you can access the scores of the bulb-dimmer interactions and view by bulb listing or by dimmer listing. The information can be sorted by column. Graphs of the light output curves, efficiency, and spectral power distribution (not yet available) of each bulb-dimmer combination are available by clicking the tab at the end of row of each table. Information on each bulb and dimmers is provided when available. There is also a tab at the top of the page to access a glossary of terms.</p></div>
+<div style="width: 180px; float: right;">
+    <b>Authors: </b>Sarah Morrissey, Dan
+Hougentogler, Greg Keane, Robert Alphin 
+    and Eric Benson<br>
+    <b>Contact Info:</b> <br>
+Dr. Eric Benson,  Robert Alphin, or Dan 
+Hougentogler<br>
+531 South College Ave. <br>
+Rm 044<br>
+Newark De 19716<br>
+(302) 831-2524<br>
+    <a href=mailto:LEDlighting@udel.edu>LEDlighting@udel.edu</a><br>
+</div>
 <p>&nbsp;</p>  <div class="disclaimer"><strong>Disclaimer</strong>-
 Information provided on this website relating to goods and services offered by third parties is provided solely for information purposes and is not intended as an endorsement or expression of support by the University of Delaware or its agents and employees. By providing this information, the University of Delaware assumes no financial or other responsibility for any relationship established by and between any company or organization listed on this website and any other individual. Any issues related to transactions that take place as a result of information provided are between the individual and the organization.</div>
- <div style="clear: both;"></div>
-
 
 </div>
 
