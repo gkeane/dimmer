fc9fe0063d8e31756ecb37d25549a22b76a012c9
Greg Keane <gkeane@gmail.com>
2016-04-26 13:52:19 -0400

    intro

--- intro.php
+++ intro.php
@@ -3,19 +3,7 @@
  <div style="height: auto; overflow: hidden;"></div>
 <div style="float: left; width: auto; overflow: hidden;">
     <h1>Introduction</h1>
-    <p>This website was designed to help broiler growers compare different light emitting diode (LED) bulbs
-
-(scientifically referred to as lamps)  tested with different dimmers to provide a score to be used as one
-
-of several factors in choosing the most appropriate LED and dimmer combinations for their broiler
-
-house(s).  Each bulb and dimmer combination is given a score of 1-100. Scores in the range of 85-97?
-
-(determine statistical range) should be considered as very good matches (statistically similar). This score
-
-should be just one of several factors when choosing a bulb and dimmer combination. Other important
-
-considerations are listed under <a href=addl.php>Additional Information.</a></p></div>
+    <p>This website was designed to help broiler growers compare different light emitting diode (LED) bulbs (scientifically referred to as lamps) tested with different dimmers to provide a score to be used as one of several factors in choosing the most appropriate LED and dimmer combinations for their broiler house(s). Each bulb and dimmer combination is given a score of 1-100.  This score should be just one of several factors when choosing a bulb and dimmer combination. Other important considerations are listed under <a href=addl.php>Additional Information.</a></p></div>
 <div style="width: 180px; float: right;">
     <b>Authors: </b>Sarah Morrissey, Dan
 Hougentogler, Greg Keane, Robert Alphin
